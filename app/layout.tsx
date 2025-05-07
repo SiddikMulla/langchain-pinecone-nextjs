9b976949e548e324931538f8d8ab2c80a3aaea35
SiddikMulla <sidmm747@gmail.com>
2025-05-08 00:26:02 +0530

    feat: enhance layout and homepage with new features and themes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,17 @@
 import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
-
+import { neobrutalism } from '@clerk/themes'
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
+    <ClerkProvider appearance={{
+      signIn: { baseTheme: neobrutalism },
+    }}>
       <html lang="en">
-        <body className="min-h-screen h-screen overflow-hidden flex">
+        <body className="min-h-screen h-screen overflow-hidden flex flex-col">
           {children}
         </body>
       </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,6 @@
+"use client"
+
+import { Button } from "@/components/ui/button";
 import {
   BrainCogIcon,
   EyeIcon,
@@ -6,6 +9,8 @@ import {
   ServerCogIcon,
   ZapIcon
 } from "lucide-react"
+import Image from "next/image";
+import Link from "next/link";
 
 const features = [
   {
@@ -45,8 +50,67 @@ export default function Home() {
     <>
       <main className="flex-1 overflow-scroll p-2 lg:p-5 bg-gradient-to-bl from-white to-indigo-600">
         <div>
-          <div className="bg-white py-24 text-center font-bold lg:text-2xl sm:py-32 rounded-md drop-shadow-xl">
-            Started pdf to chat
+          <div className="bg-white py-24 sm:py-32 rounded-md drop-shadow-xl">
+            <div className="flex flex-col justify-center items-center mx-auto max-w-7xl px-6 lg:px-8">
+              <div className="mx-auto max-w-2xl sm:text-center">
+                <h2 className="text-base font-semibold leading-7 text-indigo-600">Your Interactive Document Companion</h2>
+                <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+                  Transform Your PDFs into Interactive Conversations
+                </p>
+
+                <p className="mt-6 text-lg leading-8 text-gray-600">
+                  Introducing{" "}
+                  <span className="font-bold text-indigo-600">Chat with PDF</span>
+                  <br /><br />
+                  Upload Your document, and out chatbot will answer questions, summerise
+                  content, and answer all your Qs. Ideal for everyone,<span className="text-indigo-600">
+                    Chat with PDF</span>{" "} turns static documents into{" "}
+                  <span className="font-bold">dynamic conversation</span>,
+                  enhancing productivity 10x fold effortlessly.
+                </p>
+              </div>
+
+              <Button asChild className="mt-10">
+                <Link href='/dashboard'>Get Started</Link>
+              </Button>
+            </div>
+
+            <div className="relative overflow-hidden pt-16">
+              <div className="mx-auto max-w-7xl px-7 lg:px-8">
+                <Image
+                  alt="app screenshot"
+                  src='https://i.imgur.com/VciRSTI.jpeg'
+                  width={2432}
+                  height={1442}
+                  className="mb-[-0%] rounded-xl shadow-xl ring-1
+                 ring-gray-900/10"
+                />
+                <div aria-hidden='true' className="relative">
+                  <div className="absolute bottom-0 -inset-x-32 bg-gradient-to-t from-white/95 pt-[5%]" />
+                </div>
+              </div>
+            </div>
+
+            <div className="mx-auto mt-16 max-w-7xl px-6 sm:mt-20 md:mt-24 lg:px-8">
+              <dl className="mx-auto grid max-w-2xl grid-cols-1 gap-x-6 gap-y-10 text-base leading-7 
+              text-grey-600 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3 lg:gap-x-8 lg:gap-y-16 ">
+                {features.map((feature) => (
+                  <div key={feature.name} className="relative pl-9">
+                    <dt className="inline font-semibold text-gray-900">
+                      <feature.icon
+                        aria-hidden='true'
+                        className="absolute left-1 top-1 h-5 w-5 text-indigo-600" />
+                      <span className="font-bold text-lg">
+                        {feature.name}
+                      </span>
+                    </dt>
+                    <dd>
+                      {feature.description}
+                    </dd>
+                  </div>
+                ))}
+              </dl>
+            </div>
           </div>
         </div>
       </main>
