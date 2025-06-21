bfeb9355eb60dc0e78b1f2569baa39201d9a18ea
SiddikMulla <sidmm747@gmail.com>
2025-06-21 17:13:44 +0530

    refactor: remove unused imports from Header component

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,8 +4,6 @@ import Link from "next/link"
 import { Button } from "./ui/button"
 import { Crown, FilePlus2, Menu, X } from "lucide-react"
 import { useState } from "react"
-import Image from "next/image"
-import doc from '@/public/dv-rm.png'
 const Header = () => {
     const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false)
 
